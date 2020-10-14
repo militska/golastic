309804cea385490f0413dad77818f9a2f98156bf
militska <militska@intersvyaz.net>
2020-10-14 23:44:09 +0500

    connected)

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	host     = "postgresql"
+	port     = 5432
+	user     = "user"
+	password = "123"
+	dbname   = "db_main"
+)
 
 func main() {
 	log.Print("hi!")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
