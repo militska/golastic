91421848a51526f2b8956c1e25d1385201abbe22
militska <militska@intersvyaz.net>
2020-10-15 00:32:52 +0500

    go insert

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"log"
+	"time"
 )
 
 const (
@@ -33,4 +34,35 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected!")
+
+	go inserter(22)
+	go inserter(44)
+	go inserter(55)
+	go inserter(60)
+
+	time.Sleep(60 * time.Second)
+}
+
+func inserter(num int) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `
+INSERT INTO data (num)
+VALUES ($1)`
+
+	for i := 0; i < 2000; i++ {
+		_, err = db.Exec(sqlStatement, num)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
