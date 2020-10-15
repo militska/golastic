17d183e809b6039fc67b846590c949a33ae138c8
militska <militska@intersvyaz.net>
2020-10-15 09:42:58 +0500

    add   waitgroup

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"sync"
+	"time"
 )
 
 const (
@@ -28,14 +29,28 @@ func main() {
 
 	fmt.Println("Successfully connected!")
 
-	var wg sync.WaitGroup
+	start := time.Now()
+	// Код для измерения
+	duration := time.Since(start)
 
+	for i := 0; i < 4; i++ {
+		inserterSimple(i + 20)
+	}
+	fmt.Println("without go - " + fmt.Sprintf("%.6f", duration))
+
+	//---------------- with go
+
+	var wg sync.WaitGroup
+	startGo := time.Now()
+	// Код для измерения
+	durationGo := time.Since(startGo)
 	for i := 0; i < 4; i++ {
 		wg.Add(1)
 		go inserter(i+20, &wg)
 	}
-
 	wg.Wait()
+	fmt.Println("with go - " + fmt.Sprintf("%.6f", durationGo))
+
 }
 
 func getConnect() *sql.DB {
@@ -55,6 +70,28 @@ func inserter(num int, group *sync.WaitGroup) {
 	db := getConnect()
 	defer func() {
 		group.Done()
+		err := db.Close()
+
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	sqlStatement := `
+		INSERT INTO data (name, description)
+		VALUES ($1, $2)`
+
+	for i := 0; i < 4000; i++ {
+		_, err := db.Exec(sqlStatement, num, num)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func inserterSimple(num int) {
+	db := getConnect()
+	defer func() {
 		err := db.Close()
 
 		if err != nil {
