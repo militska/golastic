74c1031e7c58bc9e088fc295e692c817ecc5f186
militska <militska@intersvyaz.net>
2020-10-16 14:44:28 +0500

    .

--- main.go
+++ main.go
@@ -18,58 +18,20 @@ const (
 )
 
 func main() {
-	log.Print("hi!")
-	db := getConnect()
-	defer db.Close()
-
-	err := db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-
+	//---------------- without go
 	start := time.Now()
-	// Код для измерения
 	duration := time.Since(start)
-
 	for i := 0; i < 4; i++ {
 		inserterSimple(i + 20)
 	}
 	fmt.Println("without go - " + fmt.Sprintf("%.6f", duration))
 
 	//---------------- with go
-
-	var wg sync.WaitGroup
-	startGo := time.Now()
-	// Код для измерения
-	durationGo := time.Since(startGo)
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		go inserter(i+20, &wg)
-	}
-	wg.Wait()
-	fmt.Println("with go - " + fmt.Sprintf("%.6f", durationGo))
-
-}
-
-func getConnect() *sql.DB {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := sql.Open("postgres", getDsn())
 	if err != nil {
 		panic(err)
 	}
-
-	return db
-}
-
-func inserter(num int, group *sync.WaitGroup) {
-	db := getConnect()
 	defer func() {
-		group.Done()
 		err := db.Close()
 
 		if err != nil {
@@ -77,11 +39,43 @@ func inserter(num int, group *sync.WaitGroup) {
 		}
 	}()
 
+	var wg sync.WaitGroup
+	startGo := time.Now()
+	durationGo := time.Since(startGo)
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go inserter(i+20, &wg, db)
+	}
+	wg.Wait()
+	fmt.Println("with go - " + fmt.Sprintf("%.6f", durationGo))
+
+}
+
+func getDsn() string {
+	return fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+}
+
+func getConnect() *sql.DB {
+	db, err := sql.Open("postgres", getDsn())
+	db.SetMaxOpenConns(10)
+
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func inserter(num int, group *sync.WaitGroup, db *sql.DB) {
+	defer group.Done()
+
+	log.Print(db.Stats().OpenConnections)
 	sqlStatement := `
 		INSERT INTO data (name, description)
 		VALUES ($1, $2)`
 
-	for i := 0; i < 4000; i++ {
+	for i := 0; i < 1000; i++ {
 		_, err := db.Exec(sqlStatement, num, num)
 		if err != nil {
 			panic(err)
@@ -103,7 +97,7 @@ func inserterSimple(num int) {
 		INSERT INTO data (name, description)
 		VALUES ($1, $2)`
 
-	for i := 0; i < 4000; i++ {
+	for i := 0; i < 1000; i++ {
 		_, err := db.Exec(sqlStatement, num, num)
 		if err != nil {
 			panic(err)
